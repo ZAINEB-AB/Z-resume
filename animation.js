e922ea3549c30ad0b3382edb58f324a98d4f438b
ZAINEB-AB <96784428+ZAINEB-AB@users.noreply.github.com>
2023-10-16 15:52:07 +0100

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,71 +1,13 @@
-body {
-    font-family: Arial, sans-serif;
-    margin: 0;
-    padding: 0;
-    background-color: #f4f4f4;
-}
+// Redirection vers la page de contact lors du clic sur le bouton "Contacter"
+document.getElementById("contact-button").addEventListener("click", function () {
+    window.location.href = "contact.html";
+});
 
-header {
-    background-image: url('pngtree-dark-abstract-background-with-dark-overlap-layers-image_304990.jpg'); /* Image de fond pour l'en-tête */
-    background-size: cover;
-    color: #fff;
-    text-align: center;
-    padding: 20px;
-}
+// Ajout d'interactivité aux sections
+const sections = document.querySelectorAll("section");
 
-header img {
-    max-width: 150px;
-    border-radius: 50%;
-    display: block;
-    margin: 0 auto 20px;
-}
-
-h1 {
-    font-size: 36px;
-    margin: 0;
-}
-
-h2 {
-    font-size: 24px;
-    margin-top: 20px;
-}
-
-h3 {
-    font-size: 18px;
-    margin-top: 10px;
-}
-
-p {
-    font-size: 18px;
-    margin: 0;
-}
-
-ul {
-    list-style-type: none;
-    padding: 0;
-}
-
-li {
-    margin: 10px 0;
-}
-
-a {
-    color: #3498db;
-    text-decoration: none;
-}
-
-section {
-    background-image: url('pngtree-dark-abstract-background-with-dark-overlap-layers-image_304990.jpg'); /* Image de fond pour les sections */
-    background-size: cover;
-    color: #fff;
-    margin: 20px;
-    padding: 20px;
-    border-radius: 5px;
-    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
-    cursor: pointer;
-    transition: transform 0.3s ease;
-}
-
-section:hover {
-    transform: scale(1.03);
-}
+sections.forEach((section) => {
+    section.addEventListener("click", function () {
+        section.classList.toggle("active");
+    });
+});
