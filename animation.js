b5a7552c216ac8cc54edc59c11ef98ba1e09b2ec
ZAINEB-AB <96784428+ZAINEB-AB@users.noreply.github.com>
2023-10-20 15:02:29 +0100

    Update animation.js

--- animation.js
+++ animation.js
@@ -1,7 +1,9 @@
 // Redirection vers la page de contact lors du clic sur le bouton "Contacter"
 document.getElementById("contact-button").addEventListener("click", function () {
-    window.location.href = "contact.html";
+    window.location.href = "contact/contact.html"; // Modifiez le chemin d'accès relatif ici
 });
+
+
 
 // Ajout d'interactivité aux sections
 const sections = document.querySelectorAll("section");
@@ -11,4 +13,14 @@ sections.forEach((section) => {
         section.classList.toggle("active");
     });
 });
-
+// JavaScript pour afficher la section "Contact" avec un fondu
+document.getElementById("contact-button").addEventListener("click", function () {
+    const contactSection = document.querySelector(".contact");
+    contactSection.style.opacity = "0";
+    contactSection.style.display = "block";
+    // Utilisez une transition CSS pour un fondu en douceur
+    contactSection.style.transition = "opacity 0.5s";
+    setTimeout(() => {
+        contactSection.style.opacity = "1";
+    }, 10); // Laissez un petit délai pour que la transition fonctionne correctement
+});
